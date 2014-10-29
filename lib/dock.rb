d2b6b48de57dc25b76b73c45c1bc630485f17a70
ctrembath <claretrembath@ymail.com>
2014-10-29 20:13:02 +0000

    Completed Doc

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,14 +1,17 @@
 class Docking_station
+ DEFAULT_CAPACITY = 10
 
-  def initialize
+  def initialize (options = {})
+    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
     @bikes = []
   end
   
-  def bike_count?
+  def bike_count
     @bikes.count
   end
 
   def dock (bike)
+    raise "station is full" if full?
     @bikes << bike
   end
 
@@ -16,4 +19,11 @@ class Docking_station
     @bikes.delete(bike)
   end
 
+  def full?
+    bike_count == @capacity
+  end
+
+  def available_bikes
+    @bikes.reject { |bike| bike.broken? }
+  end
 end
--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -4,25 +4,46 @@ require "dock.rb"
   describe Docking_station do
 
   let (:bike) {Bike.new}
-  let (:station) {Docking_station.new}
+  let (:station) { Docking_station.new(:capacity => 20) }
+
+  def fill_station(station)
+    20.times { station.dock(Bike.new)}
+  end
 
     it "should accept bike" do
       bike = Bike.new
       station = Docking_station.new
       #expect 0 bikes
-      expect(station.bike_count?).to eq(0)
+      expect(station.bike_count).to eq(0)
       #dock bike in station
       station.dock(bike)
       #return bike to station
-      expect(station.bike_count?).to eq(1)
+      expect(station.bike_count).to eq(1)
     end
 
     it "should release bike" do
       station.dock(bike)
       station.release(bike)
-      expect(station.bike_count?).to eq(0)
+      expect(station.bike_count).to eq(0)
     end
 
+    it "should know when it's full" do
+      fill_station station
+      expect(station).to be_full
+    end
+
+    it "should not accept bike if full" do
+      fill_station station
+      expect( lambda{station.dock(bike)} ).to raise_error(RuntimeError)
+    end
+
+    it "should provide a list of bikes" do
+      working_bike, broken_bike = Bike.new, Bike.new
+      broken_bike.break!
+      station.dock(working_bike)
+      station.dock(broken_bike)
+      expect(station.available_bikes).to eq ([working_bike])
+    end
 
 
 
